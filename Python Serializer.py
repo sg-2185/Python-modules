cc35894d0c880b157d8d7a02be95e80ad05c9c5e
sg-2185 <78375970+sg-2185@users.noreply.github.com>
2022-02-17 12:51:38 -0700

    Add files via upload

---
+++ Python Serializer.py
@@ -0,0 +1,47 @@
+import pickle;
+
+
+#############################
+# Serialization of an Object
+#############################
+
+# Create a Class
+class PickleTest():
+    a = 0
+    b = 0
+
+    def __init__(self, i_a, i_b):
+        self.a = i_a
+        self.b = i_b
+
+
+# Create an Object
+Obj1 = PickleTest(1, 2)
+
+# Serialize an Object
+with open('c:\\Personal\\tinitiate\\object.pickle', 'wb') as f:
+    pickle.dump(Obj1, f)
+
+# DeSerialize File to Object
+with open('c:\\Personal\\tinitiate\\object.pickle', 'rb') as f:
+    ObjFile = pickle.load(f)
+
+print(ObjFile)
+
+print(ObjFile.a)
+print(ObjFile.b)
+
+# Serialization of a Dictionary
+
+# Create a Dict
+Dict1 = {'APPLE': 'FRUIT', 'POTATO': 'ROOT', 'OKRA': 'VEGETABLE'}
+
+# Serialize an Dict
+with open('c:\\Personal\\tinitiate\\dict.pickle', 'wb') as f:
+    pickle.dump(Dict1, f)
+
+# DeSerialize File to Object
+with open('c:\\Personal\\tinitiate\\dict.pickle', 'rb') as f:
+    DictFile = pickle.load(f)
+
+print(DictFile)
